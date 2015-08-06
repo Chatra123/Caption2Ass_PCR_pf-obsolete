ed8190f8e8a4a9e690eb28b344ddc47e8235a543
a <a>
2015-08-06 23:07:52 +0900

    change comment

--- src/Caption2Ass_PCR_pf/Caption2Ass_PCR.cpp
+++ src/Caption2Ass_PCR_pf/Caption2Ass_PCR.cpp
@@ -1305,7 +1305,7 @@ static int main_loop(CAppHandler& app, CCaptionDllUtil& capUtil, CAPTION_LIST& c
     {
       //Resync直後
       //同期バイト'G'は既に確認済み。残りの187byteを読み込む。
-      //再同期処理ではストリーム位置を戻せないのでResync直後なら処理を分ける。
+      //ストリーム位置を戻せないのでResync直後は処理を分ける。
       pbPacket[0] = 'G';
       readNum = fread(&pbPacket[1], TSPacketSize - 1, 1, app.fpInputTs);
       afterResync = false;
